f73d2635a6ec3364c65a3363f545abed6c13e27e
unknown <wilfaustino@gmail.com>
2021-04-01 09:54:59 -0300

    Layouts

--- src/TelaConfigs/TelaConfigs.js
+++ src/TelaConfigs/TelaConfigs.js
@@ -1,16 +1,67 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { View, Text, Switch, TextInput, StyleSheet, TouchableHighlight } from 'react-native'
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default (props) => {
 
-    return <SafeAreaView>
-        <Text>configs</Text>
-    </SafeAreaView>;
+    return <View style={styles.container}>
+
+        <Text style={styles.titulo}>Configurações</Text>
+        
+        <View style={styles.containerUnit}>
+            <Text>URL</Text>
+            <TextInput style={styles.inputs} />
+        </View>
+        
+        <View style={[styles.containerUnit, styles.espacamento]}>
+            <Text>Tempo de atualização</Text>
+            <TextInput style={styles.inputs} value="1" keyboardType="numeric" />
+        </View>
+
+        <TouchableHighlight style={styles.botaoSalvar}>
+            <View style={styles.botaoSalvarView}>
+                <Ionicons name={'save-outline'} size={19} color={'#888'} style={styles.botaoSalvarIcon} />
+                <Text>Salvar</Text>
+            </View>
+        </TouchableHighlight>
+
+    </View>;
 
 };
 
 const styles = StyleSheet.create({
-    appBody:{
-      
-    }
+    container:{
+        flex: 1,
+        justifyContent: 'center'
+    },
+    containerUnit:{
+        paddingHorizontal: 15
+    },
+    titulo:{
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 40
+    },
+    espacamento:{
+        marginTop: 30
+    },
+    inputs:{
+        borderBottomWidth: 1,
+        borderBottomColor: '#000'
+    },
+    botaoSalvar:{
+        marginTop: 30,
+        alignSelf: 'center',
+        backgroundColor: '#ccc',
+        paddingHorizontal: 13,
+        paddingVertical: 7
+    },
+    botaoSalvarView:{
+        flexDirection: 'row'
+    },
+    botaoSalvarIcon:{
+        marginRight: 8
+    },
 });
--- src/TelaPrincipal/TelaPrincipal.js
+++ src/TelaPrincipal/TelaPrincipal.js
@@ -1,19 +1,89 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native'
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default (props) => {
 
-    return <>
-        <Text style={styles.titulo}>Principal</Text>
+    return <View style={styles.container}>
+        
+        <Text style={styles.titulo}>Secadora IoT</Text>
+
+        <Text style={styles.status}>-</Text>
+        
         <View>
+            
+            <View>
+                <Text style={styles.subTitulo}>Temperatura</Text>
+                <Text style={styles.dado}>50 °C</Text>
+            </View>
+
+            <View>
+                <Text style={[styles.subTitulo, styles.espacamento]}>Umidade</Text>
+                <Text style={styles.dado}>50 %</Text>
+            </View>
+
+            <TouchableHighlight style={styles.botaoAtualizar}>
+                <View style={styles.botaoAtualizarView}>
+                    <Ionicons name={'sync-outline'} size={19} color={'#888'} style={styles.botaoAtualizarIcon} />
+                    <Text>Atualizar</Text>
+                </View>
+            </TouchableHighlight>
+
+            <View>
+                <Text style={styles.creditos}>Desenvolvido por Wilson Faustino Filho</Text>
+            </View>
 
         </View>
-    </>;
+    </View>;
 
 };
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center'
+    },
     titulo:{
-      
+        fontSize: 50,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+    status:{
+        fontSize: 15,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 50
+    },
+    subTitulo:{
+        fontSize: 38,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    dado:{
+        fontSize: 35,
+        textAlign: 'center',
+    },
+    espacamento:{
+        marginTop: 25
+    },
+    botaoAtualizar:{
+        marginTop: 35,
+        alignSelf: 'center',
+        backgroundColor: '#ccc',
+        paddingHorizontal: 13,
+        paddingVertical: 7
+    },
+    botaoAtualizarView:{
+        flexDirection: 'row'
+    },
+    botaoAtualizarIcon:{
+        marginRight: 8
+    },
+    creditos:{
+        textAlign: 'center',
+        color: '#888',
+        marginTop: 30,
+        fontSize: 12
     }
 });
